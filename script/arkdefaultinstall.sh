b80934360d06db64ca0cd5f7a364c7a0d1c98d2b
hyberbit <lst7120@gmail.com>
2018-04-09 16:47:23 -0300

    Update arkdefaultinstall.sh

--- script/arkdefaultinstall.sh
+++ script/arkdefaultinstall.sh
@@ -3,7 +3,7 @@ echo "running update"
 sudo apt-get -y update
 
 echo "downloading ark-deployer"
-git clone https://github.com/ArkEcosystem/ark-deployer.git 
+git clone https://github.com/leoxtationow/ark-deployer.git 
 
 echo "downloading nvm"
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
@@ -16,8 +16,8 @@ nvm install 8.9.1
 sudo apt-get install -y jq
 
 #Variables for installations
-PUBLICIP="35.196.132.138"
-GLOBALIP="35.196.132.138"
+PUBLICIP="35.196.246.92"
+GLOBALIP="0.0.0.0"
 CHAINNAME=Legion
 DATABASENAME=leg_ON
 CHAINTOKEN=Legion
@@ -31,13 +31,13 @@ REWARDPERBLOCK=10000000
 TOTALPREMINE=5000000000000000
 
 echo "Beginning ark node installation"
-~/ark-deployer/bridgechain.sh install-node --name $CHAINNAME --database $DATABASENAME --token $CHAINTOKEN --symbol $CHAINSYMBOL --node-ip $GLOBALIP --explorer-ip $PUBLICIP --forgers $CHAINFORGERS --max-votes $MAXVOTESPERWALLET --blocktime $CHAINBLOCKTIME --transactions-per-block $CHAINTRANSPERBLOCK --reward-height-start $REWARDSTART --reward-per-block $REWARDPERBLOCK --total-premine $TOTALPREMINE --autoinstall-deps --non-interactive
+~/ark-deployer/bridgechain.sh install-node --name $CHAINNAME --prefix $PREFIX --database $DATABASENAME --token $CHAINTOKEN --symbol $CHAINSYMBOL --node-ip $GLOBALIP --node-port 4100 --explorer-ip $PUBLICIP --explorer-port 4200 --forgers $CHAINFORGERS --max-votes $MAXVOTESPERWALLET --blocktime $CHAINBLOCKTIME --transactions-per-block $CHAINTRANSPERBLOCK --reward-height-start $REWARDSTART --reward-per-block $REWARDPERBLOCK --total-premine $TOTALPREMINE --autoinstall-deps 
 
 echo "Start-node for the new bridgechain"
 ~/ark-deployer/bridgechain.sh start-node --name $CHAINNAME --non-interactive
 
 echo "installing explorer"
-~/ark-deployer/bridgechain.sh install-explorer --name $CHAINNAME --token $CHAINTOKEN --node-ip $PUBLICIP --explorer-ip $PUBLICIP --skip-deps
+~/ark-deployer/bridgechain.sh install-explorer --name $CHAINNAME --token $CHAINTOKEN --node-ip $PUBLICIP --node-port 4100 --explorer-ip $PUBLICIP --explorer-port 4200 --autoinstall-deps
 
 echo "Changing IP address in ~/ark-explorer/start-explorer.sh to the all IPs"
 sed -i "s/$PUBLICIP/$GLOBALIP/g" ~/ark-explorer/start-explorer.sh
